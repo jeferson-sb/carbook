d5146af5817b97bfe19094b1fd1bd837b22d00f3
Jeferson S. Brito <jefersonsilva4k@hotmail.com>
2021-06-09 23:30:49 -0300

    feat: add category repository

---
+++ src/infra/category/MemCategoryRepository.ts
@@ -0,0 +1,25 @@
+import { v4 as uuidv4 } from 'uuid';
+
+import { Category } from '../../domain/Category';
+import { CategoryRepository } from '../../domain/CategoryRepository';
+
+export class MemCategoryRepository implements CategoryRepository {
+  private categories: Category[] = [];
+
+  getNextId(): string {
+    return uuidv4();
+  }
+
+  store(category: Category): void {
+    this.categories.push(category);
+  }
+
+  findAll(): Category[] {
+    return this.categories;
+  }
+
+  findByName(name: string): Category {
+    const category = this.categories.find((c) => c.name === name);
+    return category;
+  }
+}
