3586a8db182ba2e7e62df91b5fcdf140ffc99061
Jeferson S. Brito <jefersonsilva4k@hotmail.com>
2021-06-10 23:33:57 -0300

    chore: update instance of category repository

--- src/infra/category/MemCategoryRepository.ts
+++ src/infra/category/MemCategoryRepository.ts
@@ -6,6 +6,15 @@ import { CategoryRepository } from '../../domain/CategoryRepository';
 export class MemCategoryRepository implements CategoryRepository {
   private categories: Category[] = [];
 
+  private static instance: MemCategoryRepository;
+
+  static getInstance(): CategoryRepository {
+    if (!MemCategoryRepository.instance) {
+      MemCategoryRepository.instance = new MemCategoryRepository();
+    }
+    return MemCategoryRepository.instance;
+  }
+
   getNextId(): string {
     return uuidv4();
   }
