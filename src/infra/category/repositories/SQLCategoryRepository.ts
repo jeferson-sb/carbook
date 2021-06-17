39257a182e6abff92d0275f7f5f0e43997ea6440
Jeferson S. Brito <jefersonsilva4k@hotmail.com>
2021-06-17 13:52:41 -0300

    feat: add sql category repository

---
+++ src/infra/category/repositories/SQLCategoryRepository.ts
@@ -0,0 +1,35 @@
+import { Repository, getRepository } from 'typeorm';
+import { v4 as uuidv4 } from 'uuid';
+
+import { CategoryRepository } from '../../../domain/CategoryRepository';
+import { CategoryEntity } from '../typeorm/CategoryEntity';
+import { Category } from '../../../domain/Category';
+
+export class SQLCategoryRepository
+  implements CategoryRepository<CategoryEntity>
+{
+  private repository: Repository<CategoryEntity>;
+
+  constructor() {
+    this.repository = getRepository(CategoryEntity);
+  }
+
+  getNextId(): string {
+    return uuidv4();
+  }
+
+  async store(categoryData: Category): Promise<void> {
+    const category = this.repository.create(categoryData);
+    await this.repository.save(category);
+  }
+
+  async findAll(): Promise<CategoryEntity[]> {
+    const categories = await this.repository.find();
+    return categories;
+  }
+
+  async findByName(name: string): Promise<CategoryEntity> {
+    const category = await this.repository.findOne({ name });
+    return category;
+  }
+}
