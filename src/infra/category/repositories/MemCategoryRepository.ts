02ee3f12b22b303f26873b026056f6f04f56ba46
Jeferson S. Brito <jefersonsilva4k@hotmail.com>
2021-06-18 20:55:22 -0300

    fix: category memory repository

--- src/infra/category/repositories/MemCategoryRepository.ts
+++ src/infra/category/repositories/MemCategoryRepository.ts
@@ -3,12 +3,12 @@ import { v4 as uuidv4 } from 'uuid';
 import { Category } from '../../../domain/Category';
 import { CategoryRepository } from '../../../domain/CategoryRepository';
 
-export class MemCategoryRepository implements CategoryRepository<Category> {
+export class MemCategoryRepository implements CategoryRepository {
   private categories: Category[] = [];
 
   private static instance: MemCategoryRepository;
 
-  static getInstance(): CategoryRepository<Category> {
+  static getInstance(): CategoryRepository {
     if (!MemCategoryRepository.instance) {
       MemCategoryRepository.instance = new MemCategoryRepository();
     }
@@ -19,11 +19,11 @@ export class MemCategoryRepository implements CategoryRepository<Category> {
     return uuidv4();
   }
 
-  async store(category: Category): void {
+  async store(category: Category): Promise<void> {
     this.categories.push(category);
   }
 
-  async findAll(): Category[] {
+  async findAll(): Promise<Category[]> {
     return this.categories;
   }
 
