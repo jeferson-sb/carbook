99513999d8ad97e7c69d513e93a42358e3309091
Jeferson S. Brito <jefersonsilva4k@hotmail.com>
2021-06-26 21:08:16 -0300

    feat: add admin middleware

---
+++ src/presentation/api/middlewares/ensureAdmin.ts
@@ -0,0 +1,25 @@
+import { Request, Response, NextFunction } from 'express';
+
+import { SQLUserRepository } from '../../../infra/user/repositories/SQLUserRepository';
+
+interface UserRequest extends Request {
+  user: {
+    id: string;
+  };
+}
+
+export async function ensureAdmin(
+  request: UserRequest,
+  response: Response,
+  next: NextFunction,
+) {
+  const { id } = request.user;
+  const userRepository = new SQLUserRepository();
+  const user = await userRepository.findById(id);
+
+  if (!user.is_admin) {
+    throw new Error('User is not admin!');
+  }
+
+  return next();
+}
