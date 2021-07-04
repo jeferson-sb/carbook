209f290e624ba1228802243c27bc0c0969be9327
Jeferson S. Brito <jefersonsilva4k@hotmail.com>
2021-07-04 17:46:44 -0300

    feat: add date provider

---
+++ src/domain/DateProvider.ts
@@ -0,0 +1,5 @@
+export interface DateProvider {
+  dateNow(): Date;
+  compareInHours(startDate: Date, endDate: Date): number;
+  convertToUTC(date: Date): string;
+}
---
+++ src/infra/providers/DayjsDateProvider.ts
@@ -0,0 +1,23 @@
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+
+import { DateProvider } from '../../domain/DateProvider';
+
+dayjs.extend(utc);
+
+export class DayjsDateProvider implements DateProvider {
+  dateNow(): Date {
+    return dayjs().toDate();
+  }
+
+  compareInHours(startDate: Date, endDate: Date): number {
+    const startDateUTC = this.convertToUTC(startDate);
+    const endDateUTC = this.convertToUTC(endDate);
+
+    return dayjs(endDateUTC).diff(startDateUTC, 'hours');
+  }
+
+  convertToUTC(date: Date): string {
+    return dayjs(date).utc().local().format();
+  }
+}
