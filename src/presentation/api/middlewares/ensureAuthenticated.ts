d045883045aa08ff8259ddf1ac2e14eefd2e5a8e
Jeferson S. Brito <jefersonsilva4k@hotmail.com>
2021-09-22 09:46:19 -0300

    fix: user token middleware

--- src/presentation/api/middlewares/ensureAuthenticated.ts
+++ src/presentation/api/middlewares/ensureAuthenticated.ts
@@ -2,7 +2,6 @@ import { NextFunction, Request, Response } from 'express';
 import { verify } from 'jsonwebtoken';
 
 import { CONSTANTS } from '@config/auth';
-import { SQLUserTokensRepository } from '@modules/user/infra/repositories/SQLUserTokensRepository';
 import { HTTPError } from '../errors/HTTPError';
 
 interface JWTPayload {
@@ -29,17 +28,13 @@ export async function ensureAuthenticated(
   try {
     const { sub: userId } = verify(token, CONSTANTS.SECRET_TOKEN) as JWTPayload;
 
-    const userTokensRepository = new SQLUserTokensRepository();
-    const user = await userTokensRepository.findByUserIdAndToken(userId, token);
-
-    if (!user) throw new HTTPError('User does not exists', 400);
-
     request.user = {
       id: userId,
     };
 
     next();
   } catch (error) {
+    console.log(error.message);
     throw new HTTPError('Invalid token', 401);
   }
 }
